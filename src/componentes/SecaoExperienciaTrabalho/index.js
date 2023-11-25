ac3898dccdb31d028e0379f6ff34873effff69eb
Osvaldo Holanda <osvaldolimah@gmail.com>
2023-11-24 22:55:08 -0300

    tentativa de correcao de erros no componente card

--- src/componentes/SecaoExperienciaTrabalho/index.js
+++ src/componentes/SecaoExperienciaTrabalho/index.js
@@ -1,7 +1,10 @@
 import './style.css';
 import Card from "../Card";
+import { useState } from 'react';
 
 export default function SecaoExperienciaTrabalho(props) {
+
+  const [ehTemaEscuro, setTemaEscuro] = useState(false);
 
   return (
     <section id="secaoExpDeTrab" className={props.ehTemaEscuro ? "secao-modo-escuro" : "secao-modo-claro"}>
@@ -13,19 +16,19 @@ export default function SecaoExperienciaTrabalho(props) {
         </p>
       </div>
       <div className="container-cards limitar-secao">
-        <Card
+        <Card ehTemaEscuro={ehTemaEscuro}
         data="JUNHO 2012 - 2016"
         titulo="Web Designer"
         empresa="Pied Piper StartUp."
         paragrafo="Criação de Landing pages, sites institucionais e E-commerces completamente personalizados e otimizados para buscadores"
         />
-        <Card
+        <Card ehTemaEscuro = {ehTemaEscuro}
           data="AGOSTO 2016 - 2019"
           titulo="Product Designer"
           empresa="E Corp."
           paragrafo="Criação de modelos estratégicos de conversão identificando o cliente e mapeando toda a sua jornada de compra"
         />
-        <Card
+        <Card ehTemaEscuro = {ehTemaEscuro}
           data="FEVEREIRO 2019 - 2021"
           titulo="Digital Consulting"
           empresa="Arasaka Inc."
